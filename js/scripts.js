1b6e9974c155cc2fbecc2b1a0c9912fb5e924e13
Marcelo Glacial <glacial@gmail.com>
2017-09-19 18:03:52 -0400

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+<!--
+// função para aumentar e diminuir o texto do corpo da notícia
+var Tamanho=100;
+function TextoTamanho(funcao)
+{
+	if(funcao=="aumenta")
+	{
+		if(Tamanho<150)
+		{
+			Tamanho=Tamanho+20;
+			document.getElementById("Texto").style.fontSize = Tamanho + "%";
+		}
+	}
+	if(funcao=="diminui")
+	{
+		if(Tamanho>100)
+		{
+		Tamanho=Tamanho-20;
+		document.getElementById("Texto").style.fontSize = Tamanho + "%";			  
+		}
+	}
+}
+//-->
